309417c069f1bfc010aa8d0e852bb28adfba3ec5
Robert <robua@web.de>
2013-10-25 10:56:45 +0200

    rostansia

--- de/logit/kaiser_clone/Robert.java
+++ de/logit/kaiser_clone/Robert.java
@@ -5,4 +5,23 @@ public class Robert {
 	 * @author Robert
 	 *
 	 */
+	
+	private String rostansia;
+
+	/**
+	 * @param _rostansia
+	 */
+	public Robert(String _rostansia) {
+		rostansia = _rostansia;
+	}
+
+	public String getRostansia() {
+		return rostansia;
+	}
+
+	public void setRostansia(String _rostansia) {
+		rostansia = _rostansia;
+	}
+	
+	
 }
