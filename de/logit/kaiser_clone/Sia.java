ffa3cf37623ff5c86f155eb579a113f0f5a23bf8
siamak <siamak.hamidi421@yahoo.de>
2013-10-25 10:57:03 +0200

    hir habe ich test

--- de/logit/kaiser_clone/Sia.java
+++ de/logit/kaiser_clone/Sia.java
@@ -4,6 +4,11 @@ public class Sia
 {
 	static void main(String[] _args){
 		System.out.println("Hallo hier ist Sia");
+		System.out.println("hier mit werde ich nochmal testen" +
+				"" +
+				"ob funktioniert ");
+		
+		
 	}
 	
 	// hallo sia ist im git
