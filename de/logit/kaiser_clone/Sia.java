918f10b334689027df5b22cc09fc69d839c5b24d
siamak <siamak.hamidi421@yahoo.de>
2013-10-25 13:24:19 +0200

    Sias update
    Signed-off-by: siamak <siamak.hamidi421@yahoo.de>

--- de/logit/kaiser_clone/Sia.java
+++ de/logit/kaiser_clone/Sia.java
@@ -3,10 +3,7 @@ package de.logit.kaiser_clone;
 public class Sia
 {
 	static void main(String[] _args){
-		System.out.println("Hallo hier ist Sia");
-		System.out.println("hier mit werde ich nochmal testen" +
-				"" +
-				"ob funktioniert ");
+		System.out.println("ANDRE ANGEBER HEy heyyy");
 		
 		System.out.println("Robert testet auch");
 	}
