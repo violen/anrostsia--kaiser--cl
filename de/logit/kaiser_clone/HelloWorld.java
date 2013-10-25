93f554bac10312ea70d26406c3904bf7a529110e
André Hauser <wolfoneil@gmx.de>
2013-10-25 10:06:10 +0200

    Hallo Welt zur Git einrichtung

---
+++ de/logit/kaiser_clone/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package de.logit.kaiser_clone;
+
+/**
+ * @author nepo aka. André Hauser
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hallo Welt!");
+
+	}
+
+}
