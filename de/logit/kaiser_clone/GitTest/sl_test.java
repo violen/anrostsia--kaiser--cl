5dce216f7b13ce70a6f9864f46e9d88c67e469b8
Stephan <slc-bremen@web.de>
2013-10-30 09:31:04 +0100

    SL_Test
    noch ien test von SL

---
+++ de/logit/kaiser_clone/GitTest/sl_test.java
@@ -0,0 +1,15 @@
+package de.logit.kaiser_clone.GitTest;
+
+public class sl_test
+{
+
+	/**
+	 * @noch ein test...
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
