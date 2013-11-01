60a292f5cd56e39c56b990a6b8d9118637f3492e
André Hauser <wolfoneil@gmx.de>
2013-11-01 13:31:19 +0100

    EingabeController fertig + Diagramm mit dem Controller erweitert

--- de/logit/kaiser_clone/controller/EingabeController.java
+++ de/logit/kaiser_clone/controller/EingabeController.java
@@ -3,6 +3,8 @@
  */
 package de.logit.kaiser_clone.controller;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
@@ -11,5 +13,22 @@ import java.io.InputStreamReader;
  */
 public class EingabeController {
 	
+	private InputStreamReader isr = new InputStreamReader(System.in);
+	private BufferedReader br = new BufferedReader(isr);
+	private String eingabe;
+	
+	public EingabeController(){
+		
+	}
+
+	public String getEingabe() {
+		
+		try {
+			this.eingabe = br.readLine();
+		} catch (IOException e) {
+		}
+		
+		return eingabe;
+	}
 
 }
