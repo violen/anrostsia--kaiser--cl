f5ba56313a259824c4653772c4409b7dfaed5e42
André Hauser <wolfoneil@gmx.de>
2013-11-04 23:52:24 +0100

    Test mit Zeichenformatierungen gemacht damit wir uns evtl das "UMgeumlaute" sparen können :)

--- de/logit/kaiser_clone/GitTest/HelloWorld.java
+++ de/logit/kaiser_clone/GitTest/HelloWorld.java
@@ -3,6 +3,11 @@
  */
 package de.logit.kaiser_clone.GitTest;
 
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
 /**
  * @author nepo aka. André Hauser
  *
@@ -14,8 +19,17 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 
-		System.out.println("Hallo Sia!!!");
-
+		System.out.println("Hallo Sia!!! \t Ein Vertikaler Tab " + System.getProperty("file.encoding"));
+		System.setProperty("file.encoding", "UTF8");
+		System.out.println(System.getProperty("file.encoding"));
+		/*
+		 * Windows CMD erwartet Zeichen im DOS-Zeichensatz "CP850"
+		 */
+	    try {
+			System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out),true,"CP850"));
+		} catch (UnsupportedEncodingException e) {
+		}
+		System.out.println("Hauptmenü:: Hi ich bin André");
 	}
 
 }
