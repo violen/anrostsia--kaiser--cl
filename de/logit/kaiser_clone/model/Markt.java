37832d3f9496006fded74f5f8ffdb87861d497d2
André Hauser <wolfoneil@gmx.de>
2013-11-04 20:48:58 +0100

    Die Klasse Markt um Attribute Construktor und die Methoden "erhoeheBestaende" sowie "kaufeTitel" erweitert

--- de/logit/kaiser_clone/model/Markt.java
+++ de/logit/kaiser_clone/model/Markt.java
@@ -8,5 +8,21 @@ package de.logit.kaiser_clone.model;
  *
  */
 public class Markt {
+	private int bestandKorn = 10000;
+	private int bestandMehl = 10000;
+	
+	public Markt(){
+		
+	}
+	
+	private void erhoeheBestaende(){
+		this.bestandKorn+= 500;
+		this.bestandMehl+= 500;
+	}
+	
+	public Titel kaufeTitel(Titel _titel){
+		//TODO
+		return null;
+	}
 
 }
