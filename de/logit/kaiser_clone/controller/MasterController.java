59cacab0689f3214d7c8adad0e00cd8b7b8d369c
André Hauser <wolfoneil@gmx.de>
2013-11-05 20:29:01 +0100

    Umlaute auf der Konsole ausgeben... bis auf das kleine "ü"

--- de/logit/kaiser_clone/controller/MasterController.java
+++ de/logit/kaiser_clone/controller/MasterController.java
@@ -5,6 +5,13 @@
  */
 package de.logit.kaiser_clone.controller;
 
+import java.io.FileDescriptor;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
 /**
  * @author nepo aka. André Hauser
  *
@@ -14,7 +21,14 @@ public class MasterController {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnsupportedEncodingException {
+		/*
+		 * Zeichenausgabe Umlaute etc. für Windows CMD
+		 */
+		if(System.getProperty("file.encoding") == "Cp1252"){
+				System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out),true,"Cp850"));
+		}
+		
 		/*
 		 * Grundvorraussetzungen um mit Usereingaben umzugehen.
 		 */
