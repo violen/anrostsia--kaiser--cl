504b9d64657828d70d0e6575e4dcb7e605b83718
André Hauser <wolfoneil@gmx.de>
2013-11-05 23:52:59 +0100

    Die Methode zeigeMarkt implementiert und die Methode zeigeHauptmenü ergänzt sodass sichtbar wird das da Handlungsbedarf ist ;)

--- de/logit/kaiser_clone/view/HauptmenueView.java
+++ de/logit/kaiser_clone/view/HauptmenueView.java
@@ -10,7 +10,13 @@ package de.logit.kaiser_clone.view;
 public class HauptmenueView {
 	
 	public static void zeigeHauptmenue(){
-		System.out.print("Du befindest dich im Hauptmenü");
+		System.out.print("Du befindest dich im Hauptmenü ..blablablabl");
+	}
+	
+	public static void zeigeMarkt(){
+		System.out.print("Du befindest dich auf dem Markt. " +
+				"Hier kannst du Kaufen und Verkaufen! \n" +
+				"\t 1: Kaufen \t 2: Verkaufen");
 	}
 
 }
