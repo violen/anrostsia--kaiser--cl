949bd6495a2e954ff628d37b253f5c07401b0089
André Hauser <wolfoneil@gmx.de>
2013-11-06 23:43:54 +0100

    Der AusgabeHandler befindet sich nun im Package "controller"

---
+++ de/logit/kaiser_clone/controller/AusgabeHandler.java
@@ -0,0 +1,27 @@
+package de.logit.kaiser_clone.controller;
+
+import java.io.PrintWriter;
+/**
+ * @author André Hauser
+ * Dieser handler macht es möglich Auf der Konsole wie cmd.exe zu schreiben.
+ * Besonderheiten stellen Sonderzeichen da wo dieser Handler seine Muskeln spielen lässt.
+ */
+public class AusgabeHandler {
+	private PrintWriter pw;
+	
+	public AusgabeHandler(){
+		if(System.console() != null){
+			this.pw = System.console().writer();
+		}
+	}
+	
+	public void gibStringAnKonsole(String _string){
+		if(this.pw != null){
+			pw.print(_string);
+			pw.flush();
+		} else {
+			System.out.print(_string);
+		}
+	}
+
+}
