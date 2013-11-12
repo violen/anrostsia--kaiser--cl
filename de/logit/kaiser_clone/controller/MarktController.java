0357abdc65ed067af96013db0eedad8f4620f181
André Hauser <wolfoneil@gmx.de>
2013-11-12 17:08:27 +0100

    Dem MarktController den Markt gegeben

--- de/logit/kaiser_clone/controller/MarktController.java
+++ de/logit/kaiser_clone/controller/MarktController.java
@@ -1,8 +1,20 @@
 package de.logit.kaiser_clone.controller;
 
+import de.logit.kaiser_clone.model.Markt;
+import de.logit.kaiser_clone.view.AusgabeHandler;
+
 public class MarktController
 {
-
+	//Der "Eine" Markt der für alle gilt.
+	private Markt markt;
+	
+	private EingabeController eingabeController;
+	private AusgabeHandler ausgabeHandler;
+	
+	public MarktController(){
+		this.markt = Markt.getInstance();
+	}
+	
 	public void auswertenEingabeMarkt()
 	{
 		// TODO Auto-generated method stub
