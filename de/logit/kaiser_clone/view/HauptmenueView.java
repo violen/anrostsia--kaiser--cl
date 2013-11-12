2e67a02b6daedc1b47abf28f007d4c55cc813f41
André Hauser <wolfoneil@gmx.de>
2013-11-12 01:34:10 +0100

    Ausgaben für ein Paar menüausgaben eingefügt als Stilistische vorlage "ein zurück" mechanismus fehlt noch :)

--- de/logit/kaiser_clone/view/HauptmenueView.java
+++ de/logit/kaiser_clone/view/HauptmenueView.java
@@ -9,14 +9,38 @@ package de.logit.kaiser_clone.view;
  */
 public class HauptmenueView {
 	
-	public static void zeigeHauptmenue(){
-		System.out.print("Du befindest dich im Hauptmenü ..blablablabl");
+	public static String getHauptmenue(){
+		return "Du befindest dich im Hauptmenü. Du kannst nun Folgendes tun... "+"\n" +
+				"  "+"1: Zum Marktplatz"+"\t"+"2: Sabotiere jemanden"+"\t"+"3: Das Spielmenü zeigen"+"\n"+
+				"  "+"4: Zur Mühle"+"\t\t"+"5: Politik"+"\t\t"+"6: Spielstatistik anzeigen"+"\n"+
+				"  "+"7: Spielzug beenden" + "\n";
 	}
 	
-	public static void zeigeMarkt(){
-		System.out.print("Du befindest dich auf dem Markt. " +
+	public static String getMarktMenue(){
+		return "Du befindest dich auf dem Marktplatz. " +
 				"Hier kannst du Kaufen und Verkaufen! \n" +
-				"\t 1: Kaufen \t 2: Verkaufen");
+				"  "+"1: Kaufen "+"\t"+"2: Verkaufen"+"\n";
 	}
+	
+	public static String getSabotierenMenue(){
+		return "Du befindest dich nun in der Kaserne. " +
+				"Hier kannst du deine Soldaten befehligen!\n" +
+				"  "+"1: Rauben"+"\t"+"2: Zerstören"+"\n"+
+				"  "+"3: Vergiften"+"\t"+"4: Unruhe"+"\n";
+	}
+	
+	public static String getPolitikMenue(){
+		return "Du befindest dich im Parlament. "+"\n" +
+				"Hier können die belange des Volkes und des Staates reguliert werden." +"\n"+
+				"  "+ "1: Steuern"+"\t"+"2: Nahrungsrationen"+"\n";
+	}
+	
+	public static String getProduzierenMenue(){
+		return "Du befindest dich in der Mühle. " +
+				"Hier kannst du Mehl zur versorgung";
+		
+		// TODO ;D
+	}
+	
 
 }
--- de/logit/kaiser_clone/view/StartmenueView.java
+++ de/logit/kaiser_clone/view/StartmenueView.java
@@ -8,5 +8,9 @@ package de.logit.kaiser_clone.view;
  *
  */
 public class StartmenueView {
-
+	public static String getStartmenue(){
+		return "Du befindest dich im Startmenü von \"Anrostsias Kaiser\"." +"\n"+
+				"Möchtest du ein Neues Spiel beginnen oder die Anwendung beenden?"+"\n" +
+				"  " + "1: Neues Spiel beginnen." + "\t"+"2: Das Spiel beenden."+"\n";
+	}
 }
