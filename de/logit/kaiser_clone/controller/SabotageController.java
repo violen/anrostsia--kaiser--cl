9cc99747d9bf995661f4e7b98a61fc83bddb23dd
Robert <robua@web.de>
2013-11-13 23:16:47 +0100

    Locked Diagramm in Bearbeitung

--- de/logit/kaiser_clone/controller/SabotageController.java
+++ de/logit/kaiser_clone/controller/SabotageController.java
@@ -3,16 +3,50 @@
  */
 package de.logit.kaiser_clone.controller;
 
+import java.util.LinkedList;
+
+import de.logit.kaiser_clone.model.Spieler;
+import de.logit.kaiser_clone.view.HauptmenueView;
+
 /**
  * @author nepo aka. André Hauser
  *
  */
 public class SabotageController {
+	
+	private MasterController masterController;
+	
+	
+
+	/**
+	 * @param _masterController
+	 */
+	public SabotageController(MasterController _masterController) {
+		masterController = _masterController;
+	}
+
+
 
 	public void auswertenEingabeSabotage(String _parameter)
 	{
-		// TODO Auto-generated method stub
+		gegnerWaehlen(masterController.getAktiverSpieler());
+		masterController.getAusgabeHandler().gibStringAnKonsole(HauptmenueView.getSabotierenMenue());
 		
+	}
+
+
+
+	private LinkedList<Spieler> gegnerWaehlen(Spieler _aktiverSpieler) {
+
+		LinkedList<Spieler> gegner = new LinkedList<Spieler>();
+		for (Spieler s : masterController.getSpiel().getSpieler())
+		{
+			if ( s != _aktiverSpieler)
+			{
+				gegner.add(s);
+			}
+		}
+		return gegner;
 	}
 
 }
