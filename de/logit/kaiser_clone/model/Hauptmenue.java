1ad52943b62a4edccb43adb6ccd004db3333d90a
siamak <siamak.hamidi421@yahoo.de>
2013-11-15 13:06:26 +0100

    Hauptmenue constructor in Erste Stelle
    construktor

--- de/logit/kaiser_clone/model/Hauptmenue.java
+++ de/logit/kaiser_clone/model/Hauptmenue.java
@@ -1,12 +1,14 @@
-/**
- * 
- */
 package de.logit.kaiser_clone.model;
 
-/**
- * @author nepo aka. André Hauser
- *
- */
-public class Hauptmenue {
+import de.logit.kaiser_clone.controller.SabotageController;
+
+public class Hauptmenue
+{
+
+	private Markt markt;
+	private Sabotage sabotage;
+	private Politik politic;
+	private Statistik statistik;
+	private Produzieren produzieren;
 
 }
