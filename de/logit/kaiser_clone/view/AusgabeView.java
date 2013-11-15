a2f10ee47f54345662451a604e83fb9b2697d4ed
André Hauser <wolfoneil@gmx.de>
2013-11-15 10:46:57 +0100

    bei frageNachAnzahl "\n" eingefügt und die Methode getBestatigeAbbrechen implementiert

--- de/logit/kaiser_clone/view/AusgabeView.java
+++ de/logit/kaiser_clone/view/AusgabeView.java
@@ -5,16 +5,23 @@ package de.logit.kaiser_clone.view;
 
 /**
  * @author nepo aka. André Hauser
- *
+ * 
+ * Die Ausgabeview beinhaltet alle Ausgaben die immerwieder gebraucht werden können,
+ * und nicht in eine Spezielle View müssen.
  */
 public class AusgabeView 
 {
 	
-	
-	
 	public static String getFrageNachAnzahl()
 	{
-		return "Bitte Anzahl eingeben!";
+		return "bitte eine Anzahl eingeben..." + "\n";
 	}
+	
+	public static String getBestaetigeAbbrechen(String _aktionsName)
+	{
+		return "möchtest du wirklich " + _aktionsName +"?" + "\n" 
+				+ "  " + "1: Bestätigen" + "\t" +"2: Abbrechen" + "\n";
+	}
+	
 
 }
