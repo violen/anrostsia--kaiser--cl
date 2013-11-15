3e9b2bebd8167ad73ee09c187faaeaea6caa8590
André Hauser <wolfoneil@gmx.de>
2013-11-15 12:54:02 +0100

    Die PolitikView mit Steuern und NahrungsrationsMenue ausgestattet

--- de/logit/kaiser_clone/view/PolitikView.java
+++ de/logit/kaiser_clone/view/PolitikView.java
@@ -8,5 +8,17 @@ package de.logit.kaiser_clone.view;
  *
  */
 public class PolitikView {
+	
+	public static String getSteuernMenue()
+	{
+		return "Finanzbelange können hier reguliert werden:" + "\n" +
+				"  " + "1: Steuern erhöhen" + "\t" + "2: Steuern senken" + "\n";
+	}
+	
+	public static String getNahrungsrationsMenue()
+	{
+		return "Versorgung deiner Bevölkerung:" + "\n" +
+				"  " + "1: Ration erhöhen" + "\t" + "2: Ration senken" + "\n";
+	}
 
 }
