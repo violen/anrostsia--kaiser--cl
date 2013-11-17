ef9984ad80ce8ac525303344d389af5ad7dd280a
André Hauser <wolfoneil@gmx.de>
2013-11-17 01:55:51 +0100

    Der AusgabeHandler und der EingabeController Können nun Spieler mit Streams verheiraten und besitzen einen überladenen Construktor

--- de/logit/kaiser_clone/controller/EingabeController.java
+++ de/logit/kaiser_clone/controller/EingabeController.java
@@ -5,7 +5,12 @@ package de.logit.kaiser_clone.controller;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import de.logit.kaiser_clone.model.Spieler;
 
 /**
  * @author nepo aka. André Hauser
@@ -13,22 +18,35 @@ import java.io.InputStreamReader;
  */
 public class EingabeController {
 	
-	private InputStreamReader isr = new InputStreamReader(System.in);
-	private BufferedReader br = new BufferedReader(isr);
+	private InputStreamReader isr;
+	private BufferedReader br;
 	private String eingabe;
 	
+	private HashMap<Spieler, Scanner> inputStream = new HashMap<>();
+	
 	public EingabeController(){
-		
+		this.isr = new InputStreamReader(System.in);
+		this.br = new BufferedReader(isr);
+	}
+	
+	public EingabeController(InputStream _inputStream){
+		this.isr = new InputStreamReader(_inputStream);
+		this.br = new BufferedReader(isr);
 	}
 
 	public String getEingabe() {
-		
 		try {
 			this.eingabe = br.readLine();
 		} catch (IOException e) {
 		}
-		
 		return eingabe;
 	}
-
+	
+	public String getEingabe(Spieler _spieler){
+		return inputStream.get(_spieler).nextLine();
+	}
+	
+	public void linkSpielerMitInputStream(Spieler _spieler, Scanner _scanner){
+		this.inputStream.put(_spieler, _scanner);
+	}
 }
--- de/logit/kaiser_clone/view/AusgabeHandler.java
+++ de/logit/kaiser_clone/view/AusgabeHandler.java
@@ -1,6 +1,10 @@
 package de.logit.kaiser_clone.view;
 
+import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.util.HashMap;
+
+import de.logit.kaiser_clone.model.Spieler;
 /**
  * @author André Hauser
  * Dieser handler macht es möglich Auf der Konsole wie cmd.exe zu schreiben.
@@ -8,11 +12,17 @@ import java.io.PrintWriter;
  */
 public class AusgabeHandler {
 	private PrintWriter pw;
+	private Spieler spieler;
+	private HashMap<Spieler, PrintWriter> outputStream = new HashMap<>();
 	
 	public AusgabeHandler(){
 		if(System.console() != null){
 			this.pw = System.console().writer();
 		}
+	}
+	
+	public AusgabeHandler(OutputStream _outputStream){
+		this.pw = new PrintWriter(_outputStream);
 	}
 	
 	public void gibStringAnKonsole(String _string){
@@ -22,6 +32,15 @@ public class AusgabeHandler {
 		} else {
 			System.out.print(_string);
 		}
+	}
+	
+	public void gibStringAnKonsole(String _string, Spieler _spieler){
+		this.outputStream.get(_spieler).print(_string);
+		this.outputStream.get(_spieler).flush();
+	}
+	
+	public void linkSpielerMitOutputStream(Spieler _spieler, PrintWriter _printWriter){
+		this.outputStream.put(_spieler, _printWriter);
 	}
 
 }
