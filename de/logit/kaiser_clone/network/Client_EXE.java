d0ab4917f0266fa312e1c2f2440d723553dfd20d
André Hauser <wolfoneil@gmx.de>
2013-11-17 12:43:28 +0100

    Das client programm die Codierungsanpassung verpasst

--- de/logit/kaiser_clone/network/Client_EXE.java
+++ de/logit/kaiser_clone/network/Client_EXE.java
@@ -1,11 +1,29 @@
 package de.logit.kaiser_clone.network;
 
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
 public class Client_EXE {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		/*
+		 * Zeichenausgabe Umlaute etc. für Windows CMD.
+		 * Bevor irgendwas anderes kommt den Windows Standard auf DOS umwandeln.
+		 */
+		if(System.getProperty("file.encoding").equalsIgnoreCase("Cp1252")){
+			try {
+				System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out),true,"Cp850"));
+			} 
+			catch (UnsupportedEncodingException e) {
+			}
+		}
+		
+		//SpielClient in den Speicher laden:
 		ChatClient chatClient = new ChatClient();
 		chatClient.startClient();
 
