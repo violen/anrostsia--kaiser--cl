3c28528c72b40d2218136d97820a9ce35c67b245
André Hauser <wolfoneil@gmx.de>
2013-11-18 13:04:55 +0100

    der SpielRundenController kann alle SPielrunden verwalten

--- de/logit/kaiser_clone/controller/SpielRundenController.java
+++ de/logit/kaiser_clone/controller/SpielRundenController.java
@@ -1,7 +1,13 @@
 package de.logit.kaiser_clone.controller;
 
+import java.util.ArrayList;
+
+import de.logit.kaiser_clone.model.Spielrunde;
+
 public class SpielRundenController
 {
+	private ArrayList<Spielrunde> spielrunden = new ArrayList<>();
+	private Spielrunde aktuelleRunde;
 	private MasterController masterController;
 	
 	public SpielRundenController(MasterController _masterController) {
@@ -14,6 +20,14 @@ public class SpielRundenController
 	
 	public void setMasterController(MasterController masterController) {
 		this.masterController = masterController;
+	}
+
+	public Spielrunde getAktuelleRunde() {
+		return aktuelleRunde;
+	}
+
+	public void setAktuelleRunde(Spielrunde aktuelleRunde) {
+		this.aktuelleRunde = aktuelleRunde;
 	}
 
 }
