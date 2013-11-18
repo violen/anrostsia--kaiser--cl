03e2c507f686705943987aedc5cfe908ceb7955c
Robert <robua@web.de>
2013-11-18 14:02:58 +0100

    Berechne Moral
    attribut steuersatz für spieler

--- de/logit/kaiser_clone/model/Spielrunde.java
+++ de/logit/kaiser_clone/model/Spielrunde.java
@@ -37,12 +37,19 @@ public class Spielrunde
 		berechneKornverfall();
 		berechneProduktion();
 		berechneEreignisse();
-		berechneMoral();
-		berechneHunger();
+		
+		int wert =berechneHunger();
+		berechneSteuern();
+		berechneMoral(wert);
+	}
+
+	private int berechneSteuern() {
+		return 0;
+		// TODO Auto-generated method stub
 		
 	}
 
-	private void berechneHunger() {
+	private int berechneHunger() {
 
 
 		int soldaten = aktiverspieler.getSoldaten();
@@ -55,29 +62,46 @@ public class Spielrunde
 		
 		int verbrauch2 = bevoelkerung * ration;
 		
+		int wert = 0;
+		
+		String nachricht = "";
+		
 		if ( verbrauch1 > mehl )
 		{
 			soldaten = (int) (soldaten * (mehl / (soldaten * 2)));
-			//Ausgabe
+			//Ausgabe Hungersnot
 			aktiverspieler.setSoldaten(soldaten);
 			aktiverspieler.setMehl(0);
+			wert = 0;
 		}
 		else if ( verbrauch1 <= mehl)
 		{
 			int mehl2 = mehl - verbrauch1;
-			aktiverspieler.setMehl(mehl-mehl2);
+			aktiverspieler.setMehl(mehl2);
 		
 			if ( verbrauch2 >  mehl2)
 			{
 				//Hungersnot
 				aktiverspieler.setBevoelkerung((int)(bevoelkerung/2));
+				aktiverspieler.setMehl(0);
+				wert =  50;
+			}
+			else if ( verbrauch2 >= mehl2)
+			{
+				aktiverspieler.setMehl(mehl2-verbrauch2);
+				wert =  100;
 			}
 		}
+		return wert;
 		
 	}
 
-	private void berechneMoral() {
-		// TODO Auto-generated method stub
+	private void berechneMoral(int _wert) {
+
+		int moral = aktiverspieler.getMoral();
+		int steuer = aktiverspieler.getSteuersatz();
+		
+		moral = moral - steuer * (_wert/100);
 		
 	}
 
