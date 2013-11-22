2500322a588f3ed96080157c9554b677c41ddbcf
André Hauser <wolfoneil@gmx.de>
2013-11-22 14:09:09 +0100

    an Diagramme angepasst

--- de/logit/kaiser_clone/model/Kornspeicher.java
+++ de/logit/kaiser_clone/model/Kornspeicher.java
@@ -12,6 +12,22 @@ public class Kornspeicher extends Gebaeude
 	private final int lagerKapazitaet=5000; 
 	private int bestand;
 	
+	public Kornspeicher() 
+	{
+	}
+
+	public int getLagerKapazitaet() {
+		return lagerKapazitaet;
+	}
+	
+	public int getBestand() {
+		return bestand;
+	}
+
+	public void setBestand(int bestand) {
+		this.bestand = bestand;
+	}
+	
 	
 	
 	
