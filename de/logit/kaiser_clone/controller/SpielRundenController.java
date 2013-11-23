0b879f7b9889da52f22de9be0487beec37e1ee48
André Hauser <wolfoneil@gmx.de>
2013-11-23 18:51:08 +0100

    gewinn prüfung ab neuer runde

--- de/logit/kaiser_clone/controller/SpielRundenController.java
+++ de/logit/kaiser_clone/controller/SpielRundenController.java
@@ -3,7 +3,11 @@ package de.logit.kaiser_clone.controller;
 import java.util.ArrayList;
 
 import de.logit.kaiser_clone.model.Markt;
+import de.logit.kaiser_clone.model.Spieler;
 import de.logit.kaiser_clone.model.Spielrunde;
+import de.logit.kaiser_clone.model.Startmenue;
+import de.logit.kaiser_clone.model.Titel;
+import de.logit.kaiser_clone.view.AusgabeView;
 
 public class SpielRundenController
 {
@@ -24,6 +28,20 @@ public class SpielRundenController
 	}
 	
 	public void initNeueRunde(Spielrunde _spielrunde){
+		for(Spieler _gewinner : masterController.getSpiel().getSpieler()){
+			if(_gewinner.getTitel() == Titel.KÖNIG){
+				for(Spieler _spieler : masterController.getSpiel().getSpieler()){
+					masterController.getAusgabeHandler().gibStringAnKonsole(AusgabeView.getGewonnenHatSpieler(_gewinner.getName()), _spieler);
+				}
+				try {
+					Thread.sleep(15000);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				Startmenue.beendeSpiel();
+			}
+		}
 		this.spielrunden.add(_spielrunde);
 		this.aktuelleRunde = new Spielrunde(masterController.getSpiel().getNextSpieler(
 				masterController.getAktiverSpieler(), masterController.getSpiel().getSpieler()));
