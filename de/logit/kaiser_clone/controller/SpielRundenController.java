016c888ad4bf180fa3571f0c2e3efaa330c45e2e
André Hauser <wolfoneil@gmx.de>
2013-11-23 17:07:51 +0100

    SpielRundenController angepasst
    auch dieser soll und darf den MC nicht setzen ;D und aus dem SRC den MC
    getten darf man auch nicht :)

--- de/logit/kaiser_clone/controller/SpielRundenController.java
+++ de/logit/kaiser_clone/controller/SpielRundenController.java
@@ -12,15 +12,7 @@ public class SpielRundenController
 	private MasterController masterController;
 	
 	public SpielRundenController(MasterController _masterController) {
-		this.setMasterController(_masterController);
-	}
-	
-	public MasterController getMasterController() {
-		return masterController;
-	}
-	
-	public void setMasterController(MasterController masterController) {
-		this.masterController = masterController;
+		this.masterController = _masterController;
 	}
 
 	public Spielrunde getAktuelleRunde() {
